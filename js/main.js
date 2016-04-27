d4d38cb1a41c0c48001bddb118ada7511e21ab60
Natalee <nataleedesotell@gmail.com>
2016-04-26 19:10:39 -0500

    added Law csv
    added the Law csv, not a functional choropleth yet though

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
 (function(){
 
 //array for law (Gaby is working on the data!, categories may need to be altered)
-var arrayLaw = [ "Legal","Illegal","Moratorium"]; 
+var arrayLaw = [ "Legal","Not","Moratorium", "Formal Hold", "De Facto Moratorium"]; 
 //array for years
 var yearArray = ["1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"];
 //deleted other arrays for later because we are focusing on law/total # of executions
@@ -61,7 +61,7 @@ function setMap() {
 
         //load in the data
     d3_queue.queue()
-        .defer(d3.csv, "data/Executions/Total77to13.csv")
+        .defer(d3.csv, "data/Law.csv")
         .defer(d3.json, "data/continentalUS.topojson")
         .await(callback);
 
