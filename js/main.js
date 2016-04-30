db424bdee0cc249dfdfaf022e2e89a1b94d1d51e
nataleedesotell <nataleedesotell@gmail.com>
2016-04-29 22:04:07 -0500

    working towards choropleth
    deleted extraneous attributes from the projection, removed bootstrap
    button that we didn't need, attempted choropleth but no luck yet.
    csvData is reading as null.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 //bugs/issues to deal with:
 //1) x1 is not defined -- related to the retrievelabel. need to rectify this issue.
-//2) right now, we don't have the legal data so it's all reading as "legal" and the values are NaN
-//3) figure out how to cycle over time automatically
-//4) figure out how to create proportional symbols (raw data for # executions per state in each year)
-//5) some states do not highlight, and VA/WV highlight together :/ WHY
-
+//2) figure out how to cycle over time automatically
+//3) figure out how to create proportional symbols (raw data for # executions per state in each year)
+//4) We aren't moving the retrievelabel around so moveLabel is extraneous, but removing it makes the
+//dehighlight function break
 
 //wrap it all in an anonymous function
 (function(){
-
-//array for law (Gaby is working on the data!, categories may need to be altered)
-var arrayLaw = [ "Legal","Not","Moratorium", "Formal Hold", "De Facto Moratorium"]; 
+var topicArray = ["allExecutions", "Law"];
+//array for law
+var arrayLaw = [ "Legal","Not","Moratorium", "Formal Hold", "De Facto Moratorium"];
 //array for years
 var yearArray = ["1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"];
-//deleted other arrays for later because we are focusing on law/total # of executions
+//deleted other arrays for because we are focusing on law/total # of executions
 //if time, go back to earlier commits and grab old arrays for race, method, etc.
-
+var currentColors = [];
+var currentArray = [];
+yearExpressed = yearArray[yearArray.length-1];
 //set up initial attribute
 var expressed = arrayLaw[0];
 // dimensions of the bar graph = a function of window width
@@ -34,81 +35,116 @@ var chartWidth = window.innerWidth * 0.35,
 
 //when window loads, initiate map
 window.onload = setMap();
-
 //set up the choropleth
 function setMap() {
-
-
     // map variable, an svg element with attributes styled in style.css
     var map = d3.select("#mainmap")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
-
 //set the projection for the US, equal area because choropeth
     var projection = d3.geo.albers()
         .scale(1000)
-        .center([0.00, 39.8333333])
-        .rotate ([98.585522, 0, 0])
-        //double check these parallels at a later  time
-        .parallels([30, 48])
         .translate([width / 2, height / 2]);
-
         //path to draw the map
     var path = d3.geo.path()
         .projection(projection);
-
         //load in the data
     d3_queue.queue()
         .defer(d3.csv, "data/Law.csv")
         .defer(d3.json, "data/continentalUS.topojson")
         .await(callback);
+        //retrieve and process json file and data
+        function callback(allExecutions, Law, continentalUS){
+            //Variable to store the USA json with all attribute data
+            joinedJson = topojson.feature(continentalUS, continentalUS.objects.states).features;
+            colorize = colorScale(joinedJson);
 
-//set up callback function with 3 
-  function callback(error, csvData, us){
-    //translate WI TopoJSON using the topojson.feature() method
-    var states = topojson.feature(us, us.objects.continentalUS).features;
-    states = joinData(states, csvData);
-
-    //add color scale
-    var colorScale = makeColorScale(csvData);
-    //add enumeration units to the map
-    setEnumerationUnits(states, map, path, colorScale);
-    };
-};//end of setMap
-
-//function to join our data since we brought csv/topo in separately
-function joinData(states, csvData) {
-    //loop through csv to assign attribute values to the USstates
-    for (var i=0; i<csvData.length; i++){
-        //variable for the current county in topo
-        var csvState = csvData[i]; 
-        //variable for the csv primary key
-        var csvKey = csvState.NAME; 
-        //loop through geojson regions to find correct region
-        for (var a=0; a<states.length; a++){
-            //the current county geojson properties
-            var geojsonProps = states[a].properties;
-            //the geojson primary key
-            var geojsonKey = geojsonProps.NAME;
-            //if primary keys match, transfer csv data to geojson properties object
-            if (geojsonKey == csvKey){
-                //assign all attributes and values
-                arrayLaw.forEach(function(attr){
-                    //get csv attribute value, take it in as a string and return as a float
-                    var val = (parseFloat(csvState[attr]));
-                    //assign the attribute and its value to geojson properties
-                    geojsonProps[attr] = val; 
-                });
+            //Create an Array with CSV's loaded
+            var csvArray = [allExecutions, Law];
+            //Names for the overall Label we'd like to assign them
+            var attributeNames = ["allExecutions", "Law"];
+            //For each CSV in the array, run the LinkData function
+            for (csv in csvArray){
+                joinData(continentalUS, csvArray[csv], attributeNames[csv]);
             };
-        };
-    };
-    return states;
-};
 
-//function to set the enumeration units in the map 
+            function joinData(topojson, csvData, attribute){
+                 var jsonStates = continentalUS.objects.states.geometries;
+
+                //loop through the csv and tie it to the topojson
+                 for(var i=0; i<csvData.length; i++){
+                    var csvState = csvData[i];
+                    var csvLink = csvState.NAME;
+
+                    //loop through states and assign  data
+                    for(var a=0; a<jsonStates.length; a++){
+
+                        //check if NAME = NAME, which will join
+                        if (jsonStates[a].properties.NAME == csvLink){
+                            attrObj = {};
+                            console.log("hey"); //fails
+                            //loop to assign key/value pairs to json object
+                            for(var year in yearArray){
+                                var attr = yearArray[year];
+                                var val = (csvState[attr]);
+                                attrObj[attr] = val;
+                                console.log("hi"); //fails
+                            };
+
+
+                        jsonStates[a].properties[attribute] = attrObj;
+                        };
+                    };
+                 };
+            };
+
+            //Style the states to be styled according to the data
+            var states = map.selectAll(".states")
+                .data(joinedJson)
+                .enter()
+                .append("path")
+                .attr("class", function(d){
+                    return "states " + d.properties.NAME;
+                })
+                .style("fill", function(d){
+                    return choropleth(d, colorize);
+                })
+                .attr("d", function(d) {
+                    return path(d);
+                })
+                .on("mouseover", highlight)
+                .on("mouseout", dehighlight);
+
+            var statesColor = states.append("desc")
+                .text(function(d) {
+                    return choropleth(d, colorize);
+                })
+            changeAttribute(yearExpressed, colorize);
+        }; //END callback
+    }; //END setmap
+
+
+    function colorScale(data){
+    // this if/else statement determines which variable is currently being expressed and assigns the appropriate color scheme to currentColors
+        if (expressed === "Law") {
+            currentColors = colorArrayOverview;
+            currentArray = arrayOverview;
+        } else if (expressed === "allExecutions") {
+            currentColors = colorArrayConsent;
+            currentArray = arrayConsent;
+        };
+
+        scale = d3.scale.ordinal()
+                    .range(currentColors)
+                    .domain(currentArray); //sets the range of colors and domain of values based on the currently selected
+        return scale(data[yearExpressed]);
+    };
+
+//function to set the enumeration units in the map
 function setEnumerationUnits(states, map, path, colorScale) {
+  console.log("enum");
     //variable USstates, styled in style.css
     var USstates = map.selectAll(".USstates")
         .data(states)
@@ -155,8 +191,8 @@ function makeColorScale(data){
 
     //build two-value array of minimum and maximum expressed attribute values
     var minmax = [
-        d3.min(data, function(d) { return parseFloat(d[expressed]); }),
-        d3.max(data, function(d) { return parseFloat(d[expressed]); })
+        d3.min(data, function(d) { return (d[expressed]); }),
+        d3.max(data, function(d) { return (d[expressed]); })
     ];
     //assign two-value array as scale domain
     colorScale.domain(minmax);
@@ -232,19 +268,9 @@ function setLabel(props) {
 function moveLabel(){
     //get width of label
     var labelWidth = d3.select(".retrievelabel")
-        .node()
-        .getBoundingClientRect()
         .width;
-    //"x is not defined" related to this line, but label disappears without this line
-    //how to fix that??
-    var x = d3.event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
-    //vertical label coordinate, testing for overflow
-    var y = d3.event.clientY < 75 ? y2 : y1;
+
     d3.select(".retrievelabel")
-        .style({
-            "left": x + "px",
-            "top": y + "px"
-        });
 };
 
 })(); //last line of main.js
