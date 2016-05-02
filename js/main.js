9a938cf202b58c93c04ce29c4763db0dd7dd8c3e
kbrito <airnomadkai@gmail.com>
2016-05-02 08:20:35 -0500

    Reformatting
    We're getting close. Many test runs lead to actual development!

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ var chartWidth = window.innerWidth * 0.35,
         height = 400;
 
 //when window loads, initiate map
-window.onload = setMap2();
+window.onload = setMap3();
 
 //set up the choropleth
 function setMap() {
@@ -116,7 +116,7 @@ function setMap2() {
 
         //set the projection for the US, equal area because choropeth
         var mapSpecs = d3.geo.albers()
-            .scale(1000)
+            .scale(300)
             .center([0.00, 39.8333333])
             .rotate ([98.585522, 0, 0])
             //double check these parallels at a later  time
@@ -125,10 +125,100 @@ function setMap2() {
 
         svg.append("path")
           .datum(topojson.feature(us, us.objects.continentalUS).features)
-          .attr("d", d3.geo.path().projection(mapSpecs));
+          .attr("d", d3.geo.path().projection(d3.geo.mercator()));
 
         console.log(us.objects.continentalUS);
+
+        var projection = d3.geo.mercator()
+            .scale(500)
+            .translate([width / 2, height / 2]);
+
+        var path = d3.geo.path()
+            .projection(mapSpecs);
+
+        svg.append("path")
+            .datum(us.objects.continentalUS)
+            .attr("d", path);
+
     });
+
+}
+
+function setMap3() {
+
+
+    var formatNumber = d3.format(",.0f");
+
+    var path = d3.geo.path()
+        .projection(null);
+
+    var radius = d3.scale.sqrt()
+        .domain([0, 1e6])
+        .range([0, 15]);
+
+    var svg = d3.select("#mainmap")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    var legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")")
+      .selectAll("g")
+        .data([1e6, 5e6, 1e7])
+      .enter().append("g");
+
+    legend.append("circle")
+        .attr("cy", function(d) { return -radius(d); })
+        .attr("r", radius);
+
+    legend.append("text")
+        .attr("y", function(d) { return -2 * radius(d); })
+        .attr("dy", "1.3em")
+        .text(d3.format(".1s"));
+
+    console.log("TEST");
+
+    d3.json("data/continentalUS.topojson", function(error, us) {
+        if (error) throw error;
+
+        svg.append("path")
+              .datum(topojson.feature(us, us.objects.continentalUS).properties)
+              .attr("class", "land")
+              .attr("d", path);
+
+
+        // TEST code for testing theory
+        console.log(us.objects.continentalUS);
+
+        var display = svg.selectAll(".STUSPS")
+            .data(us)
+            .enter()
+            .append("path");
+
+        svg.append("path")
+              .datum(topojson.mesh(us, us.objects.continentalUS, function(a, b) { return a !== b; }))
+              .attr("class", "border border--state")
+              .attr("d", path);
+
+        svg.append("g")
+            .attr("class", "bubble")
+            .selectAll("circle")
+              .data(topojson.feature(us, us.objects.continentalUS).features
+                .sort(function(a, b) { return b.properties.population - a.properties.population; }))
+            .enter().append("circle")
+              .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+              .attr("r", function(d) { return radius(d.properties.population); })
+            .append("title")
+              .text(function(d) {
+                return d.properties.name
+                    + "\nPopulation " + formatNumber(d.properties.population);
+              });
+    });
+
+    d3.select(self.frameElement).style("height", height + "px");
+
 
 }
 
